8a156e420561d5cfd6c08096f356f8d52ac53be8
namelew <diogomaciel.cunha@gmail.com>
2023-05-04 08:58:55 -0300

    criando argumentos que serão atributos da classe Fruit

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  String name = "Laranja";
+  double weight = 100.2;
+  String color = "Verde e Amarela";
+  String taste = "Doce e cítrica";
+  int poolDays = 40;
+  bool isMature;
+
+  if (poolDays >= 30) {
+    isMature = true;
+  } {
+    isMature = false;
+  }
+}
