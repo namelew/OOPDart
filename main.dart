e7d25121374867015b1d41bc94bf31f4a1933585
namelew <diogomaciel.cunha@gmail.com>
2023-05-05 15:33:23 -0300

    criando uma função que recebe um parâmetro oocional que pode ser nulo

--- main.dart
+++ main.dart
@@ -5,6 +5,20 @@ void main(List<String> args) {
   String taste = "Doce e cítrica";
   int poolDays = 40;
   bool isMature = funcEstaMadura(poolDays);
+
+  mostrarMadura(name, poolDays, color: color);
+}
+
+void mostrarMadura(String name, int days, {String? color}) {
+  if (funcEstaMadura(days)) {
+    print("A $name está madura");
+  } else {
+    print("A $name não está madura");
+  }
+
+  if (color != null) {
+    print("A $name é $color");
+  }
 }
 
 bool funcEstaMadura(int days) {
